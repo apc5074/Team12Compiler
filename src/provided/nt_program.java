018b1b413ab0bda6988f03afb8cd2a7776310e97
grbe22 <113363608+grbe22@users.noreply.github.com>
2024-09-29 15:59:27 -0400

    properly stubbed out methods

--- src/provided/nt_program.java
+++ src/provided/nt_program.java
@@ -2,19 +2,34 @@ package provided;
 
 import java.util.ArrayList;
 
-public class nt_program {
+public class nt_program implements JottTree {
 
     // the string associated with this functions calling
     private ArrayList<nt_functions> callSigns;
 
     public nt_program(ArrayList<Token> tokens) {
-        if (tokens.get(0).getTokenType() != TokenType.ID_KEYWORD || !tokens.get(0).getToken().equals("Def")) {
-            System.err.println("Error! Function doesn't start with DEF!");
-            return;
+        while (!tokens.isEmpty()) {
+            nt_functions func = new nt_functions();
+            tokens = func.grantTokens(tokens);
+            if (tokens == null) {
+                return;
+            }
+            callSigns.add(func);
         }
-        
     }
     public ArrayList<nt_functions> getFuncIDs() {
         return callSigns;
+    }
+
+    public boolean validateTree() {
+        return true;
+    }
+
+    public String convertToJott() {
+        return "Yeah this function was called";
+    }
+
+    public void execute() {
+        return;
     }
 }
