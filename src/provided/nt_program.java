b63b9e55d7694c65de8d0708f4f65265053bfde1
grbe22 <113363608+grbe22@users.noreply.github.com>
2024-09-29 16:01:11 -0400

    whoops
    i dont think i needed to do any of this stuff b4

--- src/provided/nt_program.java
+++ src/provided/nt_program.java
@@ -4,23 +4,6 @@ import java.util.ArrayList;
 
 public class nt_program implements JottTree {
 
-    // the string associated with this functions calling
-    private ArrayList<nt_functions> callSigns;
-
-    public nt_program(ArrayList<Token> tokens) {
-        while (!tokens.isEmpty()) {
-            nt_functions func = new nt_functions();
-            tokens = func.grantTokens(tokens);
-            if (tokens == null) {
-                return;
-            }
-            callSigns.add(func);
-        }
-    }
-    public ArrayList<nt_functions> getFuncIDs() {
-        return callSigns;
-    }
-
     public boolean validateTree() {
         return true;
     }
