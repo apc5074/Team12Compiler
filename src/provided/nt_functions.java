3a7a8cf61880f912965aa985eac6c751127df5fa
grbe22 <113363608+grbe22@users.noreply.github.com>
2024-09-29 16:05:11 -0400

    whoops
    one more fix

--- src/provided/nt_functions.java
+++ src/provided/nt_functions.java
@@ -3,20 +3,6 @@ package provided;
 import java.util.ArrayList;
 
 public class nt_functions implements JottTree {
-    public nt_functions() {
-        
-    }
-
-    public ArrayList<Token> grantTokens(ArrayList<Token> tokens) {
-        if (tokens.get(0).getTokenType() != TokenType.ID_KEYWORD || !tokens.get(0).getToken().equals("Def")) {
-            System.err.println("Error! Function doesn't start with DEF!");
-            return null;
-        }
-        /* do stuff */
-        /* trim out all the tokens that were used */
-        /* return the updated ArrayList */
-        return null;
-    }
 
     public boolean validateTree() {
         return true;
