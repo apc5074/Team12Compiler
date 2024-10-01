fec0e535e1103b2e7add0b31a1dc26009ac55672
Shawn Broderick <spb8026@rit.edu>
2024-10-01 17:15:33 -0400

    Id Node & Return Stmt Node

--- src/parserNodes/IdNode.java
+++ src/parserNodes/IdNode.java
@@ -7,15 +7,24 @@ import provided.Token;
 
 public class IdNode implements JottTree{
 
-    public static IdNode parse(Stack<Token> tokens) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'parse'");
+    private static final Exception Exception = null;
+    private String name;
+
+    public IdNode(String name)
+    {
+        this.name = name;
+    }
+
+    public static IdNode parse(Stack<Token> tokens) throws Exception {        
+        if (!Character.isAlphabetic(tokens.get(0).getToken().indexOf(0)))
+            throw Exception;
+        String id = tokens.pop().getToken();
+        return new IdNode(id);
     }
 
     @Override
     public String convertToJott() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'convertToJott'");
+        return this.name;
     }
 
     @Override
---
+++ src/parserNodes/ReturnStmtNode.java
@@ -0,0 +1,28 @@
+package parserNodes;
+
+import provided.JottTree;
+
+public class ReturnStmtNode implements JottTree{
+
+    
+
+    @Override
+    public String convertToJott() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'convertToJott'");
+    }
+
+    @Override
+    public boolean validateTree() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'validateTree'");
+    }
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+    
+
+}
