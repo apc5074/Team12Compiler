c2ccecb93dfcb17425a454da9ff6d172f33ce4ec
grbe22 <113363608+grbe22@users.noreply.github.com>
2024-10-01 17:18:05 -0400

    Stubbed out methods for each file

--- src/parserNodes/FunctionCallNode.java
+++ src/parserNodes/FunctionCallNode.java
@@ -1,6 +1,27 @@
 package parserNodes;
 import provided.JottTree;
 
-public class FunctionCallNode {
-    
+public class FunctionCallNode implements JottTree{
+
+    public static FunctionCallNode parseFunctionCallNode() {
+        return null;
+    }
+
+    @Override
+    public String convertToJott() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'convertToJott'");
+    }
+
+    @Override
+    public boolean validateTree() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'validateTree'");
+    }
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
 }
--- src/parserNodes/NumberNode.java
+++ src/parserNodes/NumberNode.java
@@ -2,5 +2,26 @@ package parserNodes;
 import provided.JottTree;
 
 public class NumberNode implements JottTree {
-    
+
+    public static NumberNode parseNumberNode() {
+        return null;
+    }
+
+    @Override
+    public String convertToJott() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'convertToJott'");
+    }
+
+    @Override
+    public boolean validateTree() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'validateTree'");
+    }
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
 }
