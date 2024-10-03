7ac44ccb9d6e3b3aa4404d080ce1ca8a7ef20760
Shawn Broderick <spb8026@rit.edu>
2024-10-03 17:29:22 -0400

    Return Statment Node

--- src/parserNodes/ReturnStmtNode.java
+++ src/parserNodes/ReturnStmtNode.java
@@ -1,15 +1,42 @@
 package parserNodes;
 
+import java.util.Stack;
+
 import provided.JottTree;
+import provided.Token;
 
 public class ReturnStmtNode implements JottTree{
 
-    
+    private static final Exception Exception = null;
+
+    private Token returnToken;
+    private ExprNode expressionNode;
+
+    public ReturnStmtNode(Token rToken, ExprNode expr)
+    {
+        this.returnToken = rToken;
+        this.expressionNode = expr;
+    }
+
+    public static ReturnStmtNode parse(Stack<Token> tokens) throws Exception {
+        if (tokens.isEmpty())
+        {
+            throw Exception;
+        }
+        Token curToken = tokens.get(0);
+        if (curToken.getToken() == "return")
+        {
+            tokens.pop();
+            ExprNode expr = ExprNode.parse(tokens);
+            return new ReturnStmtNode(curToken, expr);
+        }
+
+    }
+
 
     @Override
     public String convertToJott() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'convertToJott'");
+        return "return " + this.expressionNode.convertToJott();
     }
 
     @Override
