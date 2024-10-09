ff0956f6c6a117d7bf3cf86d95fbe8b8e05aa3da
Aidan Costello <apc5074@g.rit.edu>
2024-10-09 15:31:35 -0400

    opNode

--- src/parserNodes/OpNode.java
+++ src/parserNodes/OpNode.java
@@ -1,35 +1,50 @@
 package parserNodes;
 import provided.*;
+import java.util.Stack;
 
 public class OpNode implements JottTree {
-    private String operator;
+    private static final Exception Exception = null;
+    private Token opToken;
 
-    public OpNode(String operator) {
-        this.operator = operator;
+    public OpNode(Token opToken) {
+        this.opToken = opToken;
+    }
+
+    private static OpNode parse(Stack<Token> tokens) throws Exception
+    {
+        if (tokens.empty())
+        {
+            throw Exception;
+        }
+        Token iToken = tokens.peek();
+
+        if(!(iToken.getToken().equals("+") || iToken.getToken().equals("-") || 
+        iToken.getToken().equals("*") || iToken.getToken().equals("/") || iToken.getToken().equals(">") ||
+        iToken.getToken().equals(">=") || iToken.getToken().equals("<") || iToken.getToken().equals("<=") ||
+        iToken.getToken().equals("==") || iToken.getToken().equals("!=")))
+        {
+            throw Exception;
+        }
+        tokens.pop();
+
+        return new OpNode(iToken);
     }
 
     @Override
     public String convertToJott() {
-        return operator;  // Just return the operator as part of the equation
+        return opToken.getToken();  // Just return the operator as part of the equation
     }
 
     @Override
     public boolean validateTree() {
         // No special validation for just the operator, but can ensure it's valid
-        return operator.equals("+") || operator.equals("-") || operator.equals("*") ||
-               operator.equals("/") || operator.equals(">") || operator.equals("<") || 
-               operator.equals(">=") || operator.equals("<=") || operator.equals("==") ||
-               operator.equals("!=");
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
     }
 
     @Override
     public void execute() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'execute'");
-    }
-
-    public String getOperator() {
-        return operator;
     }
 
 }
