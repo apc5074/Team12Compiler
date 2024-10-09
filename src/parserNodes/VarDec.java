a4b5f410945dbd5dac03ad319d98c1054e533bc4
adity <aks6596@g.rit.edu>
2024-10-09 15:49:28 -0400

    added type and var dec

--- src/parserNodes/VarDec.java
+++ src/parserNodes/VarDec.java
@@ -3,55 +3,25 @@ import java.util.Stack;
 import provided.*;
 
 public class VarDec implements JottTree {
-    private static Token varToken;
-    private static Token IDToken;
+    private static TypeNode typeNode;
+    private static IdNode IDNode;
 
 
-    public VarDec(Token typeToken, Token IDToken) {
-        this.varToken = varToken;
-        this.IDToken = IDToken;
+    public VarDec(TypeNode typeNode, IdNode IDNode) {
+        this.typeNode = typeNode;
+        this.IDNode = IDNode;
     }
 
     public static VarDec parse(Stack<Token> tokens) throws Exception {
-        if (tokens.isEmpty()) {
-            throw new Exception();
-        }
+        IdNode idNode = IdNode.parse(tokens);
+        TypeNode typeNode = TypeNode.parse(tokens);
 
-        Token typeToken = tokens.firstElement(); //dont pop yet
-        Token IDtoken = tokens.elementAt(1); // Access the second element (index 1)
-        if (!isValidType(typeToken) && !isValidText(typeToken)) {
-            throw new Exception("Syntax Error: Invalid type '" + typeToken.getToken() + "' at line " 
-                                 + typeToken.getLineNum() + " in " + typeToken.getFilename());
-        }
-        if (!isValidID(IDtoken)) {
-            throw new Exception("Syntax Error: Invalid text '" + IDtoken.getToken() + "' at line " 
-                                 + IDtoken.getLineNum() + " in " + IDtoken.getFilename());
-        }
-
-        VarDec returnNode = new VarDec(typeToken, IDtoken);
-
-        tokens.pop();
-        tokens.pop();
-        return returnNode;
+        return new VarDec(typeNode, idNode);
     }
 
     @Override
     public String convertToJott() {
-        return varToken.getToken();  
-    }
-
-    private static boolean isValidType(Token type) {
-        return (type.getTokenType() == TokenType.ID_KEYWORD);
-    }
-
-    private static boolean isValidID(Token varDec) {
-        String id = varDec.getToken();
-        return id.equals(id.toLowerCase());
-    }
-
-    private static boolean isValidText(Token type) {
-        return type.getToken().equals("Int") || type.getToken().equals("Double") 
-            || type.getToken().equals("String") || type.getToken().equals("Boolean");
+        return typeNode.convertToJott() + " " +  IDNode.convertToJott();
     }
 
     @Override
@@ -66,7 +36,7 @@ public class VarDec implements JottTree {
 
     @Override
     public String toString() {
-        return varToken.getToken();
+        return "Not implemented";
     }
 
 
