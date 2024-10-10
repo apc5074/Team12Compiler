6b5ac306ba7aa4248f98df1070972a31029ebda3
grbe22 <113363608+grbe22@users.noreply.github.com>
2024-10-10 18:50:05 -0400

    implemented ElseIfNode

---
+++ src/parserNodes/ElseIfNode.java
@@ -0,0 +1,67 @@
+package parserNodes;
+import java.util.ArrayList;
+import java.util.Stack;
+
+import provided.*;
+
+public class ElseIfNode implements JottTree {
+    BodyNode body;
+    ExprNode expr;
+
+    public ElseIfNode(BodyNode b, ExprNode e) {
+        body = b;
+        expr = e;
+    }
+
+    public static ElseIfNode parse(Stack<Token> tokens) throws Exception {
+        Token next = tokens.peek();
+        // if this token is parsed with no elseif there, it returns null. this means
+        // the if statement - which is the only one to call it - will be told "hey, there's no more elifs."
+        if (next.getTokenType() != TokenType.ID_KEYWORD | next.getToken() != "Elseif") {
+            return null;
+        }
+        // undeclared else - the next keyword is Else and an ID.
+        tokens.pop();
+        next = tokens.pop();
+        if (next.getTokenType() != TokenType.L_BRACKET) {
+            // if else is called without {
+            throw new Exception ("Syntax Error\nExpected \"[\"\n"+next.getFilename()+":"+next.getLineNum());
+        }
+        ExprNode e = ExprNode.parse(tokens);
+        next = tokens.pop();
+        if (next.getTokenType() != TokenType.R_BRACKET) {
+            // if else is called without }
+            throw new Exception ("Syntax Error\nExpected \"]\"\n"+next.getFilename()+":"+next.getLineNum());
+        }
+        next = tokens.pop();
+        if (next.getTokenType() != TokenType.L_BRACE) {
+            // if else is called without {
+            throw new Exception ("Syntax Error\nExpected \"{\"\n"+next.getFilename()+":"+next.getLineNum());
+        }
+        BodyNode e = BodyNode.parse(tokens);
+        next = tokens.pop();
+        if (next.getTokenType() != TokenType.R_BRACE) {
+            // if else is called without }
+            throw new Exception ("Syntax Error\nExpected \"}\"\n"+next.getFilename()+":"+next.getLineNum());
+        }
+        return new ElseIfNode(b, e);
+    }
+
+    @Override
+    public String convertToJott() {
+        return "Else { " + body.convertToJott() + " }";
+    }
+
+    @Override
+    public boolean validateTree() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'validateTree'");
+    }
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+    
+}
