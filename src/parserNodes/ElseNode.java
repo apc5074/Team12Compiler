c09db2ce5eb2d146533999ab4debf00af9975af4
grbe22 <113363608+grbe22@users.noreply.github.com>
2024-10-10 18:44:47 -0400

    Implemented elseNode
    I think we should check if the list is empty within these elements. it  will be a headache to implement the exception but either way's gonna be a headache too.

---
+++ src/parserNodes/ElseNode.java
@@ -0,0 +1,58 @@
+package parserNodes;
+import java.util.ArrayList;
+import java.util.Stack;
+
+import provided.*;
+
+public class ElseNode implements JottTree {
+    BodyNode body;
+    Boolean isNull;
+
+    public ElseNode(BodyNode b) {
+        isNull = false;
+        body = b;
+    }
+
+    public ElseNode() {
+        isNull = true;
+    }
+
+    public static ElseNode parse(Stack<Token> tokens) throws Exception {
+        Token next = tokens.peek()
+        if (next.getTokenType() != TokenType.ID_KEYWORD | next.getToken() != "Else") {
+            return new ElseNode();
+        }
+        // undeclared else - the next keyword is Else and an ID.
+        tokens.pop();
+        next = tokens.pop();
+        if (next.getTokenType() != TokenType.L_BRACE) {
+            // if else is called without {
+            throw new Exception ("Syntax Error\nExpected \"{\"\n"+next.getFilename()+":"+next.getLineNum());
+        }
+        BodyNode b = BodyNode.parse(tokens);
+        next = tokens.pop();
+        if (next.getTokenType() != TokenType.L_BRACE) {
+            // if else is called without }
+            throw new Exception ("Syntax Error\nExpected \"}\"\n"+next.getFilename()+":"+next.getLineNum());
+        }
+        return new ElseNode(b);
+    }
+
+    @Override
+    public String convertToJott() {
+        return "Else { " + body.convertToJott() + " }";
+    }
+
+    @Override
+    public boolean validateTree() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'validateTree'");
+    }
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+    
+}
