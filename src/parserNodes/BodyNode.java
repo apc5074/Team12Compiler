976d19860a333dfa17123c2e4d20e5be00e32392
Shawn Broderick <spb8026@rit.edu>
2024-10-14 18:00:34 -0400

    BodyNode

---
+++ src/parserNodes/BodyNode.java
@@ -0,0 +1,82 @@
+package parserNodes;
+import java.util.Stack;
+import java.util.ArrayList;
+
+import provided.JottTree;
+import provided.Token;
+import provided.TokenType;
+
+public class BodyNode implements JottTree{
+
+    private static final BodyNode Exception = null;
+
+    ArrayList<BodyStatementNode> bodyStatements;
+    ReturnStmtNode returnStmt;
+
+
+
+    public BodyNode(ArrayList<BodyStatementNode> bodyStatementNodes, ReturnStmtNode returnStmtNode)
+    {
+        this.bodyStatements = bodyStatementNodes;
+        this.returnStmt = returnStmtNode;
+    }
+
+    public BodyNode(ArrayList<BodyStatementNode> bodyStatementNodes)
+    {
+        this.bodyStatements = bodyStatementNodes;
+    }
+
+    public BodyNode parse(Stack<Token> tokens) throws Exception
+    {
+        if (tokens.empty())
+        {
+            return Exception;
+        }
+        ArrayList<BodyStatementNode> bodyStatements = new ArrayList<BodyStatementNode>();
+        Token curToken = tokens.peek();
+        while (curToken.getTokenType() != TokenType.R_BRACE && curToken.getToken() != "Return")
+        {
+            BodyStatementNode BSN = BodyStatementNode.parse(tokens);
+            bodyStatements.add(BSN);
+        }
+        if (curToken.getTokenType() == TokenType.R_BRACE)
+        {
+            return new BodyNode(bodyStatements);
+        }
+        else
+        {
+            ReturnStmtNode retrn = ReturnStmtNode.parse(tokens);
+            return new BodyNode(bodyStatements, retrn);
+        }
+
+
+    }
+
+    @Override
+    public String convertToJott() {
+        String body = "";
+        for (BodyStatementNode BSN: bodyStatements)
+        {
+            body += BSN.convertToJott();
+        }
+        if (returnStmt != null)
+        {
+            body += returnStmt.convertToJott();
+        }
+        return body;
+
+    }
+
+    @Override
+    public boolean validateTree() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'validateTree'");
+    }
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+    
+}
