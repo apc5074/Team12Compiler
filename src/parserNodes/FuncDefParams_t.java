b746994a32c9dfa5bdadd6e582622be294b35fca
a-d-i-t-y-a-s <46365777+a-d-i-t-y-a-s@users.noreply.github.com>
2024-10-16 18:27:25 -0400

    Create FuncDefParams_t.java
    implemented

---
+++ src/parserNodes/FuncDefParams_t.java
@@ -0,0 +1,54 @@
+package parserNodes;
+
+import java.util.Stack;
+import provided.JottTree;
+import provided.Token;
+
+public class FuncDefParams_t implements JottTree {
+
+    private IdNode id;
+    private TypeNode type;
+
+    public FuncDefParams_t(IdNode id, TypeNode type) {
+        this.id = id;
+        this.type = type;
+    }
+
+    public static FuncDefParams_t parse(Stack<Token> tokens) throws Exception {
+        if (tokens.isEmpty() ) {
+            // No more parameters
+            throw new Exception("Syntax Error: Null");
+        }
+        tokens.pop();
+
+        // Parse <id>
+        IdNode idNode = IdNode.parse(tokens);
+
+        // Expect ':'
+        if (tokens.isEmpty() || !tokens.peek().getToken().equals(":")) {
+            throw new Exception("Syntax Error: Expected ':' after parameter identifier");
+        }
+        tokens.pop();
+
+        // Parse <type>
+        TypeNode typeNode = TypeNode.parse(tokens);
+
+        return new FuncDefParams_t(idNode, typeNode);
+    }
+
+    @Override
+    public String convertToJott() {
+      return "," + id.convertToJott() + ":" + type.convertToJott();
+    }
+    @Override
+    public boolean validateTree() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'validateTree'");
+    }
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+}
