0c31d52bcac1d80c39efae22de8408782b27ecf7
Shawn Broderick <spb8026@rit.edu>
2024-11-09 18:03:09 -0500

    VarDec validated

--- src/parserNodes/VarDec.java
+++ src/parserNodes/VarDec.java
@@ -36,8 +36,8 @@ public class VarDec implements JottTree {
 
     @Override
     public boolean validateTree() {
-        SymbolTable.
-        return IDNode.validateTree() && typeNode.validateTree();
+        boolean notDefine = SymbolTable.addVar(IDNode.getIdToken().getToken(),typeNode);
+        return IDNode.validateTree() && typeNode.validateTree() && notDefine;
     }
 
     @Override
