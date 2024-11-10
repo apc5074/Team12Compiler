eb08212473a8b15dd6e9b8578c7b13665344bb82
Shawn Broderick <spb8026@rit.edu>
2024-11-09 19:38:24 -0500

    SymbolTable Fixed

---
+++ src/helpers/SymTabs.java
@@ -0,0 +1,11 @@
+package helpers;
+
+import java.util.HashMap;
+import java.util.List;
+
+import parserNodes.TypeNode;
+
+public class SymTabs {
+    public HashMap<String,TypeNode>  vSymTabl;
+    public HashMap<String,List<TypeNode>> fSymTabl; 
+}
